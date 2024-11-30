baba1bba6f2ff849a20d23c0bc06869899d269c0
ujinthegrey <v4deus@gmail.com>
2024-11-30 13:11:30 +0400

    bug fixing

--- js/scrollProgress.js
+++ js/scrollProgress.js
@@ -1,3 +1,3 @@
 document.addEventListener("scroll", () => {
-    document.querySelector('.scroll-progress').style.setProperty("width", ((document.documentElement.scrollTop / document.documentElement.scrollHeight) * 106).toString() + "%")
+    document.querySelector('.scroll-progress').style.setProperty("width", ((document.documentElement.scrollTop / (document.documentElement.scrollHeight - window.innerHeight)) * 100).toString() + "%")
 })
